4169e4b8f868bb27c828bda81b0e7f68c985e5b0
Aman Kalra <aman.kalra@st.ovgu.de>
2021-03-25 09:21:14 +0100

    updated ;eager loading

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -79,26 +79,26 @@ const Layout = ({ children, className = ``, thought = false }: LayoutProps) => {
             }}
           >
             <Themed.a href="https://www.linkedin.com/in/amankalra172/" sx={{ variant: `buttons.white` }} target="_blank">
-            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext"></img>
+            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext" loading="eager" ></img>
             Linkedin
             </Themed.a>
 
             <Themed.a href="https://github.com/amankalra172" sx={{ variant: `buttons.white` }} target="_blank">
-            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext"></img>
+            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext" loading="eager" ></img>
             GitHub 
             </Themed.a>
 
             <Themed.a href="https://www.facebook.com/amankalra172" sx={{ variant: `buttons.white` }} target="_blank">
-            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext"></img>
+            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext" loading="eager" ></img>
             Facebook 
             </Themed.a>
             <Themed.a href="https://www.instagram.com/amankalra172/" sx={{ variant: `buttons.white` }} target="_blank">
-            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext"></img>
+            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext" loading="eager" ></img>
             Instagram 
             </Themed.a> 
             
             <Themed.a href="https://twitter.com/amankalra172" sx={{ variant: `buttons.white` }} target="_blank">
-            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext"></img>
+            <img src="android-chrome-192x192.png"  width="22" height="22" alt="alternatetext" loading="eager" ></img>
             Twitter 
             </Themed.a> 
             
